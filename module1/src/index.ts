ac5458f18223858fa778fab2bf309d4ca7b0721f
monayemhossain90 <monayemhossain1990@gmail.com>
2023-11-04 00:20:22 +0600

    module-2 typescript basic practice

---
+++ module1/src/index.ts
@@ -0,0 +1,10 @@
+let course: string = "Next Level Development";
+console.log(course);
+const b: string = "Mostafizur rahaman";
+const age: number = 10;
+const age1: number = 6;
+const c = age + age1;
+console.log(c, b, course, age1, age);
+
+
+const school : string = "Bamni Adarsha High School"; 
